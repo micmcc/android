aed2ea7c511f63c54e7e034760174fda472f4d6e
Michael McCullough <mccullough.michael@gmail.com>
2014-03-03 21:17:43 -0700

    d

--- Sudoko/gen/com/example/sudoko/R.java
+++ Sudoko/gen/com/example/sudoko/R.java
@@ -24,7 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int about_button=0x7f080002;
+        public static final int action_settings=0x7f080004;
+        public static final int continue_button=0x7f080000;
+        public static final int exit_button=0x7f080003;
+        public static final int new_button=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -33,9 +37,14 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int about_label=0x7f050006;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int continue_label=0x7f050004;
+        public static final int exit_label=0x7f050005;
         public static final int hello_world=0x7f050002;
+        public static final int main_title=0x7f050003;
+        public static final int new_game_label=0x7f050007;
     }
     public static final class style {
         /** 
